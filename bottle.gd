d1c37c07992e5896c5505916eafae02697512374
Anna_Lehezina <anna.lehez@gmail.com>
2025-01-25 22:16:45 +0100

    carry the litter

--- bottle.gd
+++ bottle.gd
@@ -1,30 +1,36 @@
-extends AnimatedSprite2D
+extends RigidBody2D
 
 var picked = false
+var picker:Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	if picked == true:
+		self.position = picker.position
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if picked == true:
+		self.position = picker.position
+		
 
 
 func _on_area_2d_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
 	
-	print("******** bottle catched ")
 	
 	print ("body ", body, body_shape_index)
 	var bodies = $Area2D.get_overlapping_bodies()
 	print(bodies)
 	
 	if body.name == "Bubble":
+		print("******** bottle catched ")
+
+		picked = true
+		picker = body
 		body.pickLitter()
 		
-	for body1 in bodies:
-		print(body.name)
 		
 	
 	pass # Replace with function body.

262ede35f971942fa3bb13a288e6c37a9c60873a
Anna_Lehezina <anna.lehez@gmail.com>
2025-01-25 23:04:36 +0100

    ship collects litter

--- bottle.gd
+++ bottle.gd
@@ -19,18 +19,13 @@ func _process(delta: float) -> void:
 
 func _on_area_2d_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
 	
-	
 	print ("body ", body, body_shape_index)
 	var bodies = $Area2D.get_overlapping_bodies()
-	print(bodies)
+	
 	
 	if body.name == "Bubble":
 		print("******** bottle catched ")
-
 		picked = true
 		picker = body
-		body.pickLitter()
+		body.pickLitter(self)
 		
-		
-	
-	pass # Replace with function body.

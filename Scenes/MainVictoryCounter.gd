7472f0a9f3f2b04fc8ececa22212f0f863d61036
Anna_Lehezina <anna.lehez@gmail.com>
2025-01-26 11:25:44 +0100

    trashbar points

--- Scenes/MainVictoryCounter.gd
+++ Scenes/MainVictoryCounter.gd
@@ -1,6 +1,7 @@
 extends Sprite2D
 
-const victoryPoints = 0
+var victoryPoints = 0
+var bagCollected = 0
 
 
 
@@ -11,9 +12,48 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if victoryPoints>=2:
+		print("\n You win, you can swim with the fish now, little trash \n")
+	
 
 
 func _on_bottle_trash_collected(name: Variant) -> void:
 	$Trashbar/Bottle.visible = true
+	victoryPoints+=1
+	pass # Replace with function body.
+	
+
+
+func _on_boot_trash_collected(name: Variant) -> void:
+	$Trashbar/Boot.visible = true
+	victoryPoints+=1
+	pass # Replace with function body.
+	
+
+
+
+func _on_phone_trash_collected(name: Variant) -> void:
+	$Trashbar/Phone.visible = true
+	pass # Replace with function body.
+
+
+func _on_bag_2_trash_collected(name: Variant) -> void:
+	
+	bagCollected+=1
+	if bagCollected<2:
+		$Trashbar/Bag1.visible=true
+	else :
+		$Trashbar/Bag2.visible=true
+	victoryPoints+=1
+
+
+func _on_bag_1_trash_collected(name: Variant) -> void:
+	bagCollected+=1
+
+	if bagCollected<2:
+		$Trashbar/Bag1.visible=true
+	else :
+		$Trashbar/Bag2.visible=true
+	victoryPoints+=1
+	
 	pass # Replace with function body.
